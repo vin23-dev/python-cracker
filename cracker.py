243a7e1b74df472a7e2e76c32b0f85dd687d6ba8
Vincent Kokojan <vincentkokojan@vincents-mbp.attlocal.net>
2020-05-21 15:29:26 -0400

    library compare loop

--- cracker.py
+++ cracker.py
@@ -10,4 +10,16 @@ except:
     quit()
 
 
+for word in password_file:
+    
+    enc_word = word.encode('utf-8')
+    digest = hashlib.md5(enc_word.strip()).hexdigest()
 
+    if digest == password_hash:
+        print('Password found')
+        print('Password is ' + word)
+        flag = 1
+        break
+
+if flag == 0:
+    print('Password is not in the list')
